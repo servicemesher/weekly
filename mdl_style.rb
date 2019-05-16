dae56773e5708dead3a633e35e67d90ba1b3b5d0
rfma <rfma@freewheel.tv>
2019-05-16 15:11:38 +0800

    exclude rule 36 & 10

--- mdl_style.rb
+++ mdl_style.rb
@@ -5,10 +5,12 @@ rule 'MD010', :code_blocks => false
 rule 'MD013', :line_length => 160, :code_blocks => false, :tables => false
 rule 'MD026', :punctuation => ".,;:!"
 exclude_rule 'MD002'
+exclude_rule 'MD010'
 exclude_rule 'MD013'
 exclude_rule 'MD014'
 exclude_rule 'MD030'
 exclude_rule 'MD032'
 exclude_rule 'MD033'
+exclude_rule 'MD036'
 exclude_rule 'MD041'
 exclude_rule 'MD046'
